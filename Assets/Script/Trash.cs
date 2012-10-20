d07f493cd21f625e8537f3f4e8ae173699499918
Shaula <the9ball@gmail.com>
2012-10-20 14:23:48 +0900

    add new keyword

--- Assets/Script/Trash.cs
+++ Assets/Script/Trash.cs
@@ -10,7 +10,7 @@ public class Trash : Food {
 	}
 	
 	// pos : ground position
-	public static GameObject Create( Vector3 pos )
+	public static new GameObject Create( Vector3 pos )
 	{
 		Quaternion rot = Quaternion.AngleAxis( Random.Range( 0.0f, 360.0f ), new Vector3( Food.rand11(), Food.rand11(), Food.rand11() ).normalized );
 		pos.y += 1;
