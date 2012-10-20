ffc32c0fa6f95f502991b18e079788a687dfce65
umama <rustydeer@gmail.com>
2012-10-20 15:11:44 +0900

    モデルにアニメーションをちょっと追加

---
+++ Assets/LobAnimation.js
@@ -0,0 +1,23 @@
+#pragma strict
+
+var speed : float;
+
+function Start () {
+	animation["roll"].speed = speed;
+}
+
+function Fly()
+{
+	animation.Play( "fly" );
+}
+
+function Die1()
+{
+	animation.Play( "die1" );
+	yield WaitForSeconds( 3.0);
+	Destroy( gameObject );
+}
+
+function Update () { 
+
+}
