92990006964e5e464215eb22ad3cd9055e850511
Shaula <the9ball@gmail.com>
2012-10-20 14:24:23 +0900

    Add "Create Objects Count" for inspector.

--- Assets/Script/starter.cs
+++ Assets/Script/starter.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
 public class starter : MonoBehaviour {
+	[SerializeField]
+	private int m_CreateItemCount = 10;
+	
 	private const float randMax = 20.0f;
 	static public Vector3 randPos(){ return new Vector3( Random.Range(-randMax,randMax),0,Random.Range(-randMax,randMax)); }
 
 	// Use this for initialization
 	void Start () {
-		for( int i=0; i<10; ++i )
+		for( int i=0; i<m_CreateItemCount; ++i )
 		{
 			var t = Random.Range( 0, 3 );
 			switch( t )
