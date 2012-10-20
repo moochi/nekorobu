67714bf0282e9a0c5d06e716f42807697b838120
mochida <voogie01+github@gmail.com>
2012-10-20 22:56:33 +0900

    change kan drop script

--- Assets/Script/DropObjectScript.cs
+++ Assets/Script/DropObjectScript.cs
@@ -15,11 +15,20 @@ public class DropObjectScript : MonoBehaviour {
 		if (Input.GetMouseButtonDown(0))
 		{
 			Vector3 vec = Input.mousePosition;
-			vec.z = 10f;
+			/*
+			//vec.z = 10f;
 			//Debug.Log(vec);
- 
 			//Instantiate(prefab, camera.ScreenToWorldPoint(vec), Quaternion.identity);
 			Food.Create(camera.ScreenToWorldPoint(vec));
+			*/
+			
+			//vec.z = 100f;
+			//Vector3 toPos = camera.ScreenToWorldPoint(vec);
+			Ray ray = camera.ScreenPointToRay(vec);
+			RaycastHit hit;
+			if(Physics.Raycast(ray,out hit,100)) {
+				Food.Create(hit.point);
+			}
 		}
 		if (Input.GetMouseButtonDown(1))
 		{
